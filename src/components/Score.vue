60bd8892723f1f510cc3517e95354e9f5e38b7b3
TymekB <baran.tymoteusz@gmail.com>
2020-05-25 15:51:39 +0200

    added redrawScore method

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,7 +1,11 @@
 <template>
-    <div id="score">
+    <div>
+        <div id="score">
 
+
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -9,26 +13,29 @@
 
     export default {
         name: "Score",
-        data: function() {
+        data: function () {
             return {
-                vf: null,
-                score: null,
-                system: null
+                elementId: 'score'
             }
         },
         methods: {
+            redrawScore() {
+                const div = document.querySelector('#' + this.elementId);
+                [].slice.call(div.children).forEach(child => div.removeChild(child));
+            },
             drawNote(note) {
-                const vf = new Vex.Flow.Factory({renderer: {elementId: 'score', height: 150, width: 300}});
+                this.redrawScore();
+
+                const vf = new Vex.Flow.Factory({renderer: {elementId: this.elementId, height: 150, width: 300}});
                 const score = vf.EasyScore();
                 const system = vf.System();
 
                 system.addStave({
-                    voices: [score.voice(score.notes(note+ '/1'))]
+                    voices: [score.voice(score.notes(note + '/1'))]
                 }).addClef('treble').addTimeSignature('4/4');
 
                 vf.draw();
             },
-
         },
         mounted() {
             this.drawNote('C3');
